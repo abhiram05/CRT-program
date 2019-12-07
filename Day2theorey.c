6e1a8eee52ad7ad1a9f208435da38736a5bb0ae1
abhiram05 <58410800+abhiram05@users.noreply.github.com>
2019-12-07 12:25:33 +0530

    Add files via upload

---
+++ Day2theorey.c
@@ -0,0 +1,36 @@
+Control Structures types :
+
+A.Conditional control structures :-
+1)for loop
+2)while loop
+3)do-while loop
+
+B.Unconditional control structures :-
+4)break
+5)continue
+
+C.Unconditional uncontrol structures :-
+6)goto
+
+Syntax of for loop :-
+for(initialization;condition;increment/decrement)
+    statement;
+for(initialization;condition;increment/decrement)
+{
+statement-1;
+statement-2;
+------------statement-x;
+}
+
+Syntax of while loop :-
+while(condition)
+{
+	//statements;
+	}    
+
+Syntax of do-while loop :-
+do
+{
+	//statements
+}
+while(condition);
